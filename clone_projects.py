736e8918d48ccb25145a7dcd6523c5e30bbe1858
igor.davidenko <igor.davidenko@jetbrains.com>
2022-08-19 19:03:20 +0200

    bug fix

--- clone_projects.py
+++ clone_projects.py
@@ -3,8 +3,8 @@ import os
 import pandas as pd
 from tqdm import tqdm
 
-apache = pd.read_csv("/home/igor/PycharmProjects/datasets/python-1000/apache.csv")
-mit = pd.read_csv("/home/igor/PycharmProjects/datasets/python-1000/mit.csv")
+apache = pd.read_csv("./apache.csv")
+mit = pd.read_csv("./mit.csv")
 
 projects = pd.concat([apache, mit], ignore_index=True)
 projects.sort_values(by="stargazers", ascending=False, inplace=True)
